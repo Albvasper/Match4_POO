057b526ea2703a272725f3bf606b83bc4d58b2c4
Alberto Vasquez <albert.vp09@gmail.com>
2019-12-03 09:42:29 -0600

    Included SDLttf and SDLimage

--- Match4_POO/Match4_POO.cpp
+++ Match4_POO/Match4_POO.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <SDL.h>
 #include <stdio.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
@@ -13,7 +15,7 @@ int main(int argc, char* args[]) {
 		return 1;
 	}
 	window = SDL_CreateWindow(
-		"hello_sdl2",
+		"Match4 - AlbertoVasquezPerez",
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 		SCREEN_WIDTH, SCREEN_HEIGHT,
 		SDL_WINDOW_SHOWN
@@ -25,7 +27,7 @@ int main(int argc, char* args[]) {
 	screenSurface = SDL_GetWindowSurface(window);
 	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
 	SDL_UpdateWindowSurface(window);
-	SDL_Delay(2000);
+	SDL_Delay(100000);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
